c720876a5ccf4726bbf51d8fc26f3a133bf96f27
ANJANA-MANI <anjanajmani333@gmail.com>
2024-04-18 14:56:20 +0530

    emp_server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer=require('json-server')
 const empServer=jsonServer.create()
 const middleware=jsonServer.defaults()
 const router=jsonServer.router("db.json")
-const PORT=300||process.env.PORT
+const PORT=3000||process.env.PORT
 empServer.use(middleware)
 empServer.use(router)
 empServer.listen(PORT,()=>{
